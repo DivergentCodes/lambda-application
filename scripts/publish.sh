5b1fa0657f8db7f3c8cde26dd7c7931eea8a1cd3
Jack Sullivan <jack@divergent.codes>
2025-08-11 00:27:47 -0700

    feat(gha): show S3 publish path

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-dist_path="dist"
 tag="${1}"
 sha256="${2}"
+dist_path="dist"
 
 if [ -z "$tag" ] || [ "$tag" = "none" ]; then
     echo "Valid tag is required (got: '$tag')"
     exit 1
-fi
-
-if [ -z "$sha256" ] || [ "$sha256" = "none" ]; then
+elif [ -z "$sha256" ] || [ "$sha256" = "none" ]; then
     echo "Valid SHA256 hash is required (got: '$sha256')"
     exit 1
-fi
-
-echo "Publishing distribution artifacts to GitHub Release $tag"
-
-# Ensure distribution artifacts exist. Add more files if you have them.
-if [ ! -d "$dist_path" ]; then
+elif [ ! -d "$dist_path" ]; then
     echo "Distribution directory $dist_path does not exist"
     exit 1
 fi
 
-ls -lah "$dist_path"
+function publish_github_release() {
+    # Upload distribution artifacts to GitHub Release (idempotent if re-run).
+    echo "Publishing distribution artifacts to GitHub Release $tag"
+    gh release upload "$tag" "$dist_path/*" --clobber
+}
 
-# Upload distribution artifacts to GitHub Release (idempotent if re-run).
-gh release upload "$tag" "$dist_path/*" --clobber
+function publish_s3_object() {
+    bucket_path="lambda/lambda-application/$tag/$sha256/lambda.zip"
+    echo "Publishing distribution artifacts to S3 bucket $bucket_path"
+}
+
+function main() {
+    ls -lah "$dist_path"
+
+    publish_github_release
+}
+
+main
