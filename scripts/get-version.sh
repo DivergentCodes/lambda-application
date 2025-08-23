2e0bf8d40c5552f39b6922bcaf0a489fae54eadc
Jack Sullivan <jack@divergent.codes>
2025-08-23 12:38:05 -0700

    Use existing token if present

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 
 # A dummy token is set to avoid the missing token warning. We just need the version.
-version="$(GH_TOKEN="dummy" uv run semantic-release version --print)"
+export GH_TOKEN="${GH_TOKEN:-dummy}"
+version="$(uv run semantic-release version --print)"
 echo "$version"
