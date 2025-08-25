63bbfe34b0971af759d151820d90549550bb036b
Jack Sullivan <jack@divergent.codes>
2025-08-24 20:20:22 -0700

    fix(gha): no commit during GitHub release

--- scripts/release-github.sh
+++ scripts/release-github.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # This script is used to create a GitHub release.
-# - Tag the commit and push it to GitHub.
+# - Tag the commit and push the tag to GitHub.
 # - Create a GitHub release with assets.
-# - Update the changelog.
+# - Include the changelog in the release notes.
 
 set -euo pipefail
 
 uv run semantic-release version \
-    --skip-build \
-    --push
+  --skip-build \    # Built in another task.
+  --no-commit \     # Work with main branch protection settings.
+  --no-changelog \  # A changelog body will still be included in the release notes.
+  --push \          # Push the tag to GitHub (not a commit).
+  --vcs-release     # Create a GitHub release.
